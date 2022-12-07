b1a1fc4fff9b85905e8e88a986d6b6a6ab90e5eb
ThePoptartCrpr <ThePoptartCrpr@users.noreply.github.com>
2022-12-06 22:26:11 -0800

    Day 7
    janky ass solution god damb

---
+++ Day 7/day-7-1.js
@@ -0,0 +1,64 @@
+const fs = require('fs');
+const utils = require('../utils/utils.js');
+
+fs.readFile('./Day 7/input/7-terminal.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+    } else {
+        let lines = data.split('\n');
+
+        var storage = { contents: {} };
+        let currentDir = storage.contents;
+        let path = [];
+
+        for (line of lines) {
+            if (line.startsWith('$ ')) {
+                if (line.startsWith('cd', 2)) {
+                    dir = line.substring(5);
+                    if (dir != '/') {
+                        if (dir == '..') {
+                            path.pop();
+                            currentDir = storage.contents;
+                            for (folder of path) {
+                                currentDir = currentDir[folder].contents;
+                            }
+                        } else {
+                            path.push(dir);
+                            currentDir = currentDir[dir].contents;
+                        }
+                    }
+                }
+            } else if (line != '') {
+                if (line.startsWith('dir ')) {
+                    if (!currentDir[line.substring(4)]) currentDir[line.substring(4)] = ({ name: line.substring(4), type: 'dir', contents: {} });
+                } else if (!currentDir[line.split(' ')[1]]) currentDir[line.split(' ')[1]] = ({ name: line.split(' ')[1], type: 'file', size: parseInt(line.split(' ')[0]) });
+            }
+        }
+
+        let writeFolderSize = (folder) => {
+            let size = 0;
+            for ([k, item] of Object.entries(folder.contents)) {
+                if (item.type == 'file') size += item.size;
+                else size += writeFolderSize(item);
+            }
+            folder.size = size;
+            return size;
+        }
+
+        writeFolderSize(storage);
+
+        let size = 0;
+        let findFolders = (folder) => {
+            for ([k, item] of Object.entries(folder.contents)) {
+                if (item.type == 'dir') {
+                    if (item.size <= 100000) size += item.size;
+                    findFolders(item);
+                }
+            }
+        }
+
+        findFolders(storage);
+
+        utils.formatAnswer(`The total size of all directories below 100,000 bytes is <${size}>.`);
+    }
+});
